3e86b1a2486117ff66b4cbf55f72dcd0c1c81138
dilumsampath <dilumsampathkoodagoda@gmail.com>
2024-06-07 20:48:28 +0530

    Add image change function according to the weather condition and add display "error" msg when type wrong city name

--- script.js
+++ script.js
@@ -3,17 +3,44 @@ const apiUrl =
   "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  var data = await response.json();
 
-  console.log(data);
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
 
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "./weather-app-img/images/rain.png";
+    }
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "./weather-app-img/images/clouds.png";
+    }
+    if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "./weather-app-img/images/drizzle.png";
+    }
+    if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "./weather-app-img/images/mist.png";
+    }
+    if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "./weather-app-img/images/clear.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
 }
 
 searchBtn.addEventListener("click", () => {
